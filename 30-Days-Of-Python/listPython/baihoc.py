7aa0df5247e6bb38ac6c0b582334330a84180ac8
phuctrank81 <131422071+phuctrank81@users.noreply.github.com>
2025-12-06 13:31:22 +0700

    pull

---
+++ 30-Days-Of-Python/listPython/baihoc.py
@@ -0,0 +1,22 @@
+# my_list = [12, 3.5, "Hello", True, None]
+
+# print(type(my_list))
+
+# my_list = [12, 3.5, "Hello", True, [5, 6, 7]]
+# print(len(my_list))  Output: 5
+# print(my_list[0])    Output: 12
+# print(my_list[1:])
+# print(my_list[:-1])   Output: [5, 6, 7]
+
+# my_list = [12, 3.5, "Hello", True, [5, 6, 7]]
+# for x in my_list:
+#     print(x, end=" ")
+
+# list_1 = [1, 4.5, 10]
+# list_2 = ["anh", "em"]
+# concat_list = list_1 + list_2
+# print(concat_list)
+
+list_phuc = [1, 4.5, 10]
+list_phuc[1] = "hello"
+print(list_phuc)  # Output: [1, 'hello', 10]
--- 30-Days-Of-Python/logicalOperator/baitap2.py
+++ 30-Days-Of-Python/logicalOperator/baitap2.py
@@ -1,3 +1,14 @@
 """
+Kiểm tra xem có được vào rạp chiếu phim không
 
 """
+
+age = 40
+has_ticket = True
+has_parent = True
+
+# điều kiện để vào xem phim
+# phải có vé phim và tuổi từ 18 trở lên hoặc bằng 18
+# phải vừa có thỏa mãn về vé , vừa thỏa mãn về tuổi tác
+can_enter = has_ticket and (age >= 18 or has_parent)
+print("Được vào rạp chiếu phim:", can_enter)
