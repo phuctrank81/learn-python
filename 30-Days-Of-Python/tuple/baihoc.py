6fa26f0d1db6a0afa3b18d87a5070dd7c87ac7fd
phuctrank81 <131422071+phuctrank81@users.noreply.github.com>
2025-12-09 16:39:17 +0700

    tuple

--- 30-Days-Of-Python/tuple/baihoc.py
+++ 30-Days-Of-Python/tuple/baihoc.py
@@ -11,4 +11,28 @@ for x in tuple_2:
 tuple_3 = (4,"phuc dep trai", 2, 3)
 tuple_4 = ("anh", "em",43,51)
 new_tuple = tuple_3 + tuple_4
-print(new_tuple)  # Output: (4, 'phuc dep trai', 2)
+print(f"New tuple: {new_tuple}")  # Output: (4, 'phuc dep trai', 2)
+
+tuple_5 = (1,2,3,4,5, "bac")
+list_1 = list(tuple_5) 
+# list_1.append("phuc tran")
+
+list_1.remove("bac")
+new_tuple_5 = tuple(list_1)
+print(f"new tuple : {new_tuple_5}")  # Output: (1, 2, 3, 4, 5, 'bac', 'phuc tran')
+
+
+tuple_10 = (145,24,32,524)
+print(tuple_10.count(24))
+
+
+tuple_11 = ("a", 124, 524, "b")
+tuple_12 = ("a", 124, 524, "b")
+
+print(f"ID of tuple_11: {id(tuple_11)}")
+print(f"ID of tuple_12: {id(tuple_12)}")
+
+tuple_13 = ([142,5,24],"a", 124, 524, "b")
+
+tuple_13[0][0] = 0
+print(tuple_13)  # Output: ('z', 124, 524, 'b')
