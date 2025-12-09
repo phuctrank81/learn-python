74986d4acdc7b00e10ed669d0809b5eedfeb5a9a
phuctrank81 <131422071+phuctrank81@users.noreply.github.com>
2025-12-09 15:48:56 +0700

    new update

--- 30-Days-Of-Python/listPython/baihoc.py
+++ 30-Days-Of-Python/listPython/baihoc.py
@@ -17,6 +17,40 @@
 # concat_list = list_1 + list_2
 # print(concat_list)
 
-list_phuc = [1, 4.5, 10]
-list_phuc[1] = "hello"
-print(list_phuc)  # Output: [1, 'hello', 10]
+# list_phuc = [1, 4.5, 10]
+# list_phuc[1] = "hello"
+# print(list_phuc)  # Output: [1, 'hello', 10]
+
+# 1. add list element using append()
+# 2. add element .extend()
+# 3. sort list .sort() or sorted()
+# 4. reverse list .reverse()
+# 5  insert element .insert(index, value)
+# 6. delete element del or .remove()
+# 7. trả về index đầu tiên của elemnet được khớp .index() (Ko có error)
+# 8. pop(index) ko truyền xóa ele cuối 
+
+""" 1 """
+my_lst = [1,2,3]
+my_lst.append(4)
+print(my_lst)  # Output: [1, 2, 3, 4]
+
+""" 2 """
+phucTran = [10,2,4]
+phucTran.extend(["phuc"])
+print(phucTran)  # Output: [10, 2, 4, 'phuc']
+
+""" 3 """
+trangTran = [4,-4,-23,45]
+trangTran.sort()
+print(trangTran)  # Output: [-23, -4, 4, 45]
+
+newTrang = sorted(trangTran)
+print(newTrang)  # Output: [-23, -4, 4, 45]
+
+""" 4 """
+locTran = [1,4,5,-5]
+# locTran.reverse()
+print(locTran)  # Output: [-5, 5, 4, 1]
+new_list = locTran[::-1]
+print(new_list)  
